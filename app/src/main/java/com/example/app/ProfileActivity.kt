3f0c8d7f11be5aa1cac14f1f934a1696e04ff1c7
Nikita0703 <nikita.noviczenko2005@gmail.com>
2025-02-03 19:29:49 +0300

    a

--- app/src/main/java/com/example/app/ProfileActivity.kt
+++ app/src/main/java/com/example/app/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.example.app
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,10 +12,10 @@ import com.example.app.R.id.backButton
 import com.example.app.entity.Phone
 
 class ProfileActivity : AppCompatActivity() {
-    private lateinit var textViewBrand: TextView
-    private lateinit var textViewModel: TextView
-    private lateinit var textViewOs: TextView
-    private lateinit var textViewStorage: TextView
+    private lateinit var editTextName : EditText
+    private lateinit var editTextSurname : EditText
+    private lateinit var editTextEmail : EditText
+    private lateinit var editTextBirthday: EditText
     private lateinit var backButton: Button
 
     @SuppressLint("SetTextI18n")
@@ -22,10 +23,10 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        textViewBrand= findViewById(R.id.textViewBrand);
-        textViewModel= findViewById(R.id.textViewModel);
-        textViewOs= findViewById(R.id.textViewOs);
-        textViewStorage = findViewById(R.id.textViewStorage);
+        editTextName = findViewById(R.id.editTextName );
+        editTextSurname = findViewById(R.id.editTextSurname );
+        editTextEmail = findViewById(R.id.editTextEmail );
+        editTextBirthday = findViewById(R.id.editTextBirthday);
         backButton = findViewById(R.id.backButton)
 
         backButton.setOnClickListener{back()}
@@ -34,10 +35,10 @@ class ProfileActivity : AppCompatActivity() {
         val phone = Phone("Novichenko", "Nikita", "Nikita@gmail.com", "07.03.2005")
 
         // Установка данных в TextView
-        textViewBrand.text = "Бренд: ${phone.brand}"
-        textViewModel.text = "Модель: ${phone.model}"
-        textViewOs.text = "ОС: ${phone.os}"
-        textViewStorage.text = "Хранилище: ${phone.storage}GB"
+        editTextName.text = Editable.Factory.getInstance().newEditable("${phone.brand}")
+        editTextSurname.text = Editable.Factory.getInstance().newEditable("${phone.model}")
+        editTextEmail.text = Editable.Factory.getInstance().newEditable("${phone.os}")
+        editTextBirthday.text = Editable.Factory.getInstance().newEditable("${phone.storage}")
     }
 
     private fun back() {
