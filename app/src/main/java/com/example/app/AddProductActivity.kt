15b1a1b5ba59bb42b9b6fd82d098a7f59cc199b4
Nikita0703 <nikita.noviczenko2005@gmail.com>
2025-02-05 18:10:55 +0300

    a

--- app/src/main/java/com/example/app/AddProductActivity.kt
+++ app/src/main/java/com/example/app/AddProductActivity.kt
@@ -1,13 +1,20 @@
 package com.example.app
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.app.entity.Phone
 import com.example.app.repository.ProductRepository
+import java.io.File
+import java.io.FileOutputStream
 
 class AddProductActivity  : AppCompatActivity() {
     private lateinit var brandEditText: EditText
@@ -16,6 +23,9 @@ class AddProductActivity  : AppCompatActivity() {
     private lateinit var storageEditText: EditText
     private lateinit var addPhoneButton: Button
     private lateinit var backButton: Button
+    private lateinit var imageView: ImageView
+
+    private val IMAGE_PICK_CODE = 1000
 
 
     private val phoneRepo = ProductRepository() // Предполагается, что у вас есть класс PhoneRepository
@@ -31,14 +41,45 @@ class AddProductActivity  : AppCompatActivity() {
         storageEditText = findViewById(R.id.storageEditText)
         addPhoneButton = findViewById(R.id.addPhoneButton)
         backButton = findViewById(R.id.backButton)
+        imageView = findViewById(R.id.imageView)
 
         backButton.setOnClickListener{back()}
+
+        val selectImageButton: Button = findViewById(R.id.selectImageButton)
+        selectImageButton.setOnClickListener {
+            //pickImageFromGallery()
+            pickImageFromDownloads()
+        }
 
         // Установка обработчика нажатия на кнопку
         addPhoneButton.setOnClickListener {
             addPhone()
         }
     }
+
+   /* private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    */
+   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+       super.onActivityResult(requestCode, resultCode, data)
+       if (requestCode == IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK) {
+           val imageUri: Uri? = data?.data
+           imageView.setImageURI(imageUri) // Отобразить выбранное изображение
+       }
+   }
+
+           private fun pickImageFromDownloads() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*" // Указываем, что мы хотим выбрать изображение
+            putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png")) // Можно указать конкретные типы изображений
+        }
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
 
     private fun addPhone() {
         val brand = brandEditText.text.toString()
@@ -65,6 +106,8 @@ class AddProductActivity  : AppCompatActivity() {
                 Toast.makeText(this, "Ошибка при добавлении телефона: ${exception.message}", Toast.LENGTH_SHORT).show()
             }
         )
+
+        saveImageToStorage()
     }
 
     private fun clearFields() {
@@ -78,5 +121,34 @@ class AddProductActivity  : AppCompatActivity() {
         val intent = Intent(this, ProductActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun saveImageToStorage() {
+        val brandName = brandEditText.text.toString()
+
+        // Получите изображение из ImageView (предполагаем, что оно уже выбрано)
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = imageView.drawingCache
+
+        // Создайте папку для хранения изображений, если она не существует
+        val directory = File(filesDir, "images")
+        if (!directory.exists()) {
+            directory.mkdirs()
+        }
+
+        // Сохраните изображение в файл с именем из поля brandEditText
+        try {
+            val file = File(directory, "$brandName.png")
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            imageView.isDrawingCacheEnabled = false // Отключить кэширование
+            // Вы можете добавить уведомление об успешном сохранении
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // Обработка ошибок
+        }
     }
 }
